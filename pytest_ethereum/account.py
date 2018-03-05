b60b7b02c35da366d2a99be1abee12b4ac41c83c
Bryant <fubuloubu@gmail.com>
2018-03-04 23:14:43 -0500

    Added str method; needed eq/ne methods overriden so it performed string comparision

--- pytest_ethereum/account.py
+++ pytest_ethereum/account.py
@@ -9,6 +9,15 @@ class Account:
     def __repr__(self):
         return self._address
 
+    def __str__(self):
+        return self.__repr__()
+
+    def __ne__(self, other):
+        return self.__repr__().__ne__(other)
+
+    def __eq__(self, other):
+        return self.__repr__().__eq__(other)
+
     # Send Ether
     def send(self, address, amount):
         self._w3.eth.sendTransaction({'to': address, 'from': self._address, 'value': amount})
