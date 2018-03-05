b30a1081e80d4e6d350b2ef3747641408346c7ab
Bryant <fubuloubu@gmail.com>
2018-03-04 19:23:26 -0500

    Uncommented pytest specialty stuff; adding missing config for upload; changed version

--- setup.py
+++ setup.py
@@ -7,26 +7,31 @@ def read_md(fname):
 
 setup(
     name="pytest-ethereum",
-    version='0.1.0',
+    version='0.0.1a1',
     description='py.test plugin for testing Ethereum smart contracts',
     license='MIT',
     long_description=read_md('README.md'),
     author='Bryant Eisenbach',
+    author_email='bryant@dappdevs.org',
+    url='https://github.com/fubuloubu/pytest-ethereum',
+    python_requires='>=3.6',
     packages = ['pytest_ethereum'],
     install_requires=[
         'pytest>=3.4.1',
         'web3>=4.0.0b11',
     ],
+
     # the following makes a plugin available to pytest
-    #entry_points = {
-    #    'pytest11': [
-    #        'name_of_plugin = pytest_ethereum.pluginmodule',
-    #    ]
-    #},
+    entry_points = {
+        'pytest11': [
+            'name_of_plugin = pytest_ethereum.pluginmodule',
+        ]
+    },
 
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.6',
         # custom PyPI classifier for pytest plugins
         "Framework :: Pytest",
     ],
